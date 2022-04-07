8cd392e4a09783a0c748be65746fa340c16ccd65
rofl0r <rofl0r@users.noreply.github.com>
2022-04-07 03:51:53 +0000

    import freebsd patch-osd_freebsd_cd.c
    https://cgit.freebsd.org/ports/tree/emulators/hugo/files/patch-osd_freebsd_cd.c
    
    this concludes the patches from freebsd.

--- osd_freebsd_cd.c
+++ osd_freebsd_cd.c
@@ -38,7 +38,7 @@ void osd_cd_close()
 }
 
 
-int osd_cd_read(UChar *p, UInt32 sector)
+void osd_cd_read(UChar *p, UInt32 sector)
 {
 /*
   int retries = 0;
@@ -236,15 +236,3 @@ void osd_cd_play_audio_range(UChar min_from, UChar sec_from, UChar fra_from,
     perror("play_audio_range");
 */
 }
-
-void osd_cd_subchannel_info(unsigned short offset)
-{ }
- 
-void osd_cd_status(int *status)
-{}
-
-void osd_cd_resume(void)
-{}
-
-void osd_cd_pause(void)
-{}
