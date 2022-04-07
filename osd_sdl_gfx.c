3a7a9688e4232482db3d3369215271657d5af733
rofl0r <rofl0r@users.noreply.github.com>
2022-04-07 03:50:33 +0000

    import freebsd patch-osd__sdl__gfx.c

--- osd_sdl_gfx.c
+++ osd_sdl_gfx.c
@@ -18,7 +18,7 @@ int blit_x,blit_y;
 int screen_blit_x, screen_blit_y;
 // where on the screen we must blit XBuf
 
-UChar* XBuf;
+extern UChar* XBuf;
 // buffer for video flipping
 
 UChar index_to_RGB[256];
