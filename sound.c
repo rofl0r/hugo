73654bcbab3afa11b3414ded6653184ac73aad3b
rofl0r <rofl0r@users.noreply.github.com>
2022-04-07 03:46:30 +0000

    import patch-sound.c from freebsd
    https://cgit.freebsd.org/ports/tree/emulators/hugo/files/patch-sound.c

--- sound.c
+++ sound.c
@@ -50,7 +50,7 @@ UInt32 CycleNew;
 // indicates the last time music has been "released"
 
 /* TODO */
-int BaseClock=7170000;
+extern int BaseClock; // use value set in pce.c
 // int BaseClock = 8992000;
 // the freq of the internal PC Engine CPU
 // the sound use a kind of "relative" frequency
